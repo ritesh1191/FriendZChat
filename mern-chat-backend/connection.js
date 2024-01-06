d1766223ad455f07afac8477b3e9a2fe2d0e58af
Ritesh Thakare <riteshthakare1191@gmail.com>
2024-01-06 19:29:22 +0530

    Update connection.js

--- mern-chat-backend/connection.js
+++ mern-chat-backend/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose.connect(
-  `mongodb+srv://ritesh1191:8975096933@cluster0.xyy7dox.mongodb.net/mernChat?retryWrites=true&w=majority`,
+  `mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@cluster0.xyy7dox.mongodb.net/mernChat?retryWrites=true&w=majority`,
   () => {
     console.log("connected to mongodb");
   }
